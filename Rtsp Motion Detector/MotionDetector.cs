deea5fbe479fbffc358d6851e17ebafabfb96256
Wael Al-Haddad <wael.had.sy@gmail.com>
2023-08-05 13:46:27 +0400

    code refactoring

--- Rtsp Motion Detector/MotionDetector.cs
+++ Rtsp Motion Detector/MotionDetector.cs
@@ -7,92 +7,90 @@ namespace Rtsp_Motion_Detector
 {
     internal class MotionDetector
     {
-        readonly string streamUrl;
-        readonly int sensitivity;
-        bool isDetecting = false;
-        Thread? detectorThread;
+        public readonly string StreamUrl;
+        public readonly int Sensitivity;
+        public bool IsDetecting { get; private set; } = false;
+        public event EventHandler? MotionDetected;
 
-        public MotionDetector(string streamUrl, int senitivity)
+        private Thread? detectorThread;
+
+        public MotionDetector(string streamUrl, int senitivity = 5)
         {
-            this.streamUrl = streamUrl;
-            if (senitivity <= 0 || senitivity > 10)
-            {
-                this.sensitivity = 5;
-            }
-            else
-            {
-                this.sensitivity = senitivity;
-            }
+            StreamUrl = streamUrl;
+            Sensitivity = Math.Max(5, Math.Min(10, senitivity));
         }
 
         public void Start(bool showCamera = false)
         {
-            detectorThread = new Thread(() => StartDetector(showCamera: showCamera));
+            detectorThread = new Thread(() => StartDetector(showCamera: showCamera)) { IsBackground = true };
             detectorThread.Start();
         }
 
         private void StartDetector(bool showCamera = false)
         {
-            isDetecting = true;
+            IsDetecting = true;
 
             try
             {
-                Mat frame = new(), gray = new(), staticFrame = new(),
-                frameDelta = new(), thresh = new();
+                Mat frame = new(),
+                    gray = new(),
+                    staticFrame = new(),
+                    frameDelta = new(),
+                    thresh = new();
                 VectorOfVectorOfPoint cnts = new();
                 Size kSize = new(21, 21);
                 int framesCount = 0;
                 bool motionDetected = false;
 
                 //passing 0 uses the local webcam instead of a network stream
-                VideoCapture capture = new(streamUrl);
-                
+                VideoCapture capture = new(StreamUrl);
+
                 //initializing the first frame
                 capture.Read(frame);
 
                 //convert the frame to grayscale
-                CvInvoke.CvtColor(frame, staticFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                CvInvoke.CvtColor(frame, staticFrame, ColorConversion.Bgr2Gray);
                 CvInvoke.GaussianBlur(staticFrame, staticFrame, kSize, 0);
 
                 while (capture.Read(frame))
                 {
                     //checking if the detector is still running
-                    if (isDetecting == false) { break; }
+                    if (!IsDetecting)
+                        break;
 
                     framesCount++;
 
                     //convert the frame to grayscale
-                    CvInvoke.CvtColor(frame, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                    CvInvoke.CvtColor(frame, gray, ColorConversion.Bgr2Gray);
                     CvInvoke.GaussianBlur(gray, gray, kSize, 0);
 
                     if (framesCount == 10)
                     {
                         //reinitializing the static frame every 10 frames
-                        CvInvoke.CvtColor(frame, staticFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                        CvInvoke.CvtColor(frame, staticFrame, ColorConversion.Bgr2Gray);
                         CvInvoke.GaussianBlur(staticFrame, staticFrame, kSize, 0);
                         framesCount = 0;
                     }
 
                     //compute the difference between the static frame and the current frame
                     CvInvoke.AbsDiff(staticFrame, gray, frameDelta);
-                    CvInvoke.Threshold(frameDelta, thresh, 25, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+                    CvInvoke.Threshold(frameDelta, thresh, 25, 255, ThresholdType.Binary);
 
-                    //CvInvoke.Dilate(thresh, thresh, new Mat(), new Point(-1, -1), 2, Emgu.CV.CvEnum.BorderType.Constant, new MCvScalar(255, 255, 255));
+                    //CvInvoke.Dilate(thresh, thresh, new Mat(), new Point(-1, -1), 2, BorderType.Constant, new MCvScalar(255, 255, 255));
 
                     CvInvoke.FindContours(thresh, cnts, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
 
                     for (int i = 0; i < cnts.Size; i++)
                     {
-                        if (CvInvoke.ContourArea(cnts[i]) < (sensitivity * 100))
-                        {
+                        if (CvInvoke.ContourArea(cnts[i]) < (Sensitivity * 100))
                             continue;
-                        }
                         motionDetected = true;
                     }
 
                     if (motionDetected)
                     {
-                        Console.Write("MotionDetected");
+                        // Raise the motion detected event
+                        OnMotionDetected(EventArgs.Empty);
                         motionDetected = false;
                     }
                     if (showCamera)
@@ -101,18 +99,19 @@ namespace Rtsp_Motion_Detector
                         CvInvoke.Imshow("Camera", frame);
                     }
 
-                    if (CvInvoke.WaitKey(1) == 27) { break; }
+                    if (CvInvoke.WaitKey(1) == 27) 
+                        break;
                 }
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
-
         }
 
-        public void Stop()
+        // Helper method to raise the MotionDetected event
+        private void OnMotionDetected(EventArgs e)
         {
-            isDetecting = false;
-
+            MotionDetected?.Invoke(this, e);
         }
-       
+
+        public void Stop() => IsDetecting = false;
     }
 }
--- Rtsp Motion Detector/Program.cs
+++ Rtsp Motion Detector/Program.cs
@@ -2,9 +2,32 @@
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        MotionDetector motionDetector = new(streamUrl: "http://158.58.130.148/mjpg/video.mjpg", senitivity: 5);
-        motionDetector.Start(showCamera:true);
+        // Replace "YOUR_RTSP_STREAM_URL" with the actual RTSP stream URL you want to monitor
+        string rtspStreamUrl = "http://158.58.130.148/mjpg/video.mjpg";
+
+        // Create a new instance of MotionDetector
+        MotionDetector motionDetector = new(rtspStreamUrl);
+
+        // Subscribe to the MotionDetected event
+        motionDetector.MotionDetected += MotionDetector_MotionDetected;
+
+        // Start motion detection
+        motionDetector.Start(showCamera: true);
+
+        Console.WriteLine("Motion detection started. Press any key to stop...");
+        Console.ReadKey();
+
+        // Stop motion detection when a key is pressed
+        motionDetector.Stop();
+    }
+
+    // Event handler for MotionDetected event
+    private static void MotionDetector_MotionDetected(object? sender, EventArgs e)
+    {
+        Console.WriteLine("Motion detected!");
+        // You can add any custom action you want to perform when motion is detected.
+        // For example, you can send a notification, save the video clip, etc.
     }
 }
